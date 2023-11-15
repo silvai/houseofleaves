ca6a1b1ebedf539ceaafdf518fa0fd5957501f7f
Ella Silva 😱 <issilv@microsoft.com>
2023-11-15 14:55:31 -0500

    add annotations for I

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
-document.write("AM")
-document.getElementsByName("p").setAttribute("transparency", 50);
+function changeColor() {
+  var mes = document.getElementById("mes");
+  mes.style.fontSize = "45";
+  mes.style.color = "green";
+}
+
+
+function displayDate() {
+    document.getElementById("mes").style.color = "green";
+  }
+
+function randomFooter() {
+    var footerPadding = Math.random() * 100;
+    var padValue = footerPadding + "%";
+    var footer = document.getElementsByClassName("footer");
+    footer[0].style.paddingBottom = padValue;
+}
